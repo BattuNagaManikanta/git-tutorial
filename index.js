35c1f0998c22d2c26a12debbd58d307a2576d964
Naga Manikanta <maniking22000@gmail.com>
2025-11-01 14:34:57 +0530

    Image Slider version 1

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ let slider = document.querySelector('.image-container')
 let container = document.querySelector('.container');
 let dots = document.querySelector('.dots');
 
+console.log("version 1");
+
 console.log(leftButton,rightButton);
 let i = 0;
 
